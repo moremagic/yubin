bcfb97164fc03d79a2d6d494458dca7ee97a1728
moremagic <itoumagic@gmail.com>
2019-05-02 08:28:51 +0900

    main.lisp の編集

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,23 @@
-(defpackage yubin
-  (:use :cl))
-(in-package :yubin)
+(defpackage #:yubin ;
+  (:use #:cl)
+  (:import-from #:janathan #:parse)
+  (:export #:get-place))
+(in-package #:yubin)
+
+(defun get-place (zipcode) ;
+  (let* ((url (quri:make-uri :defalts "http://zipcloud.ibsnet.co.jp/api/search"
+                             :query '(("zipcode" . ,zipcode)))) ; QueryでURLを作る
+         (response (parse (dex:get url)))                       ; HTTPでリクエストを行う
+         (result (first (getf response :|results|))))
+    (if result
+        (concatenate 'string
+                     (getf result :|address1|)
+                     (getf result :|address2|)
+                     (getf result :|address3|))
+        (error format nil "~A: ~S (code=~A)"
+               (getf response :|message|)
+               zipcode
+               (getf response :|status|)))))
+
 
 ;; blah blah blah.
